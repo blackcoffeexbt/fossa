cdd387f21a1b630b92801f0fe8f4c86602f20d30
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2024-07-03 16:05:42 +0100

    Text centered :) Fuck yeah!

--- accessPointFOSSA/accessPointFOSSA.ino
+++ accessPointFOSSA/accessPointFOSSA.ino
@@ -3,29 +3,22 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
-
 TFT_eSPI tft = TFT_eSPI();
 
-
-
-void setup()  
-{  
+void setup()
+{
   Serial.begin(115200);
   tft.init();
   tft.setRotation(1);
   tft.invertDisplay(false);
   tft.fillScreen(TFT_BLACK);
 
-tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_PURPLE);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
   tft.setTextSize(3);
-
-  tft.setCursor(40, 40);
-  tft.println("Please wait while");
-  tft.setCursor(40, 100);
-  tft.println("your voucher is printed.");
-
+  tft.setTextDatum(MC_DATUM);
+  tft.drawString("Please wait while your", tft.width() / 2, tft.height() / 2 - 20);
+  tft.drawString("voucher is printed.", tft.width() / 2, tft.height() / 2 + 20);
 }
-
 
 void loop() {}
